936fc0023eb1caa5d593836a2f24b2d4e3ae7092
BigMurloc <s21159@pjwstk.edu.pl>
2022-12-15 19:05:26 +0100

    in range line implementation

--- song/song_descriptor.py
+++ song/song_descriptor.py
@@ -48,7 +48,23 @@ class SongDescriptor:
 
 
     def describe_verse_in_range(self, left, right):
-        pass
+        if not isinstance(left, int) or not isinstance(right, int):
+            raise TypeError
+        if left <= 0 or right > self._number_of_lines or right < left:
+            raise ValueError
+
+        if left == right:
+            return self.describe_single_verse(left)
+
+        result = ""
+        for i in range(left - 1, right):
+            result += self._array_of_verses[i].strip()
+            if i != (right - 1):
+                result += "\n"
+
+
+        return result
+
 
 
 class SongDescriptorTest(unittest.TestCase):
@@ -124,21 +140,21 @@ class SongDescriptorTest(unittest.TestCase):
 
     def test_describe_verse_in_range(self):
         descriptor = SongDescriptor()
-        verses = """
-        On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.
-        
-        On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-        
-        On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-        
-        On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-        """
+        verses = """On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.
+On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."""
         self.assertEqual(descriptor.describe_verse_in_range(2, 5), verses)
 
     def test_describe_verse_in_range_when_left_is_negative(self):
         descriptor = SongDescriptor()
         with self.assertRaises(ValueError):
             descriptor.describe_verse_in_range(-5, 2)
+
+    def test_describe_verse_in_range_when_left_is_zero(self):
+        descriptor = SongDescriptor()
+        with self.assertRaises(ValueError):
+            descriptor.describe_verse_in_range(0, 2)
 
     def test_describe_verse_in_range_when_right_is_negative(self):
         descriptor = SongDescriptor()
@@ -148,7 +164,7 @@ class SongDescriptorTest(unittest.TestCase):
     def test_describe_verse_in_range_when_right_is_smaller(self):
         descriptor = SongDescriptor()
         with self.assertRaises(ValueError):
-            descriptor.describe_verse_in_range("4", 1)
+            descriptor.describe_verse_in_range(4, 1)
 
     def test_describe_verse_in_range_when_range_is_equal(self):
         descriptor = SongDescriptor()
